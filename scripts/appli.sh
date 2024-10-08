e838d20edf4f620d81fceabf0a1cbed107684287
laster13 <laster13012@gmail.com>
2024-10-08 09:37:54 +0200

    fix appli.sh

--- scripts/appli.sh
+++ scripts/appli.sh
@@ -1,59 +1,63 @@
 #!/bin/bash
 
-  source /home/${USER}/seedbox-compose/profile.sh
-  get_architecture
+source /home/laster13/seedbox-compose/profile.sh
 
-  # Chemin vers le fichier JSON
-  json_file="/home/${USER}/projet-riven/riven/data/settings.json"
+json_file="/home/laster13/projet-riven/riven-frontend/static/settings.json"
 
-  # Vérifie si le fichier existe
-  if [ ! -f "$json_file" ]; then
-      echo "Le fichier $json_file n'existe pas."
-      exit 1
-  fi
 
-  # Extraire les valeurs du fichier JSON et les assigner à des variables avec jq
-  username=$(jq -r '.utilisateur.username' "$json_file")
-  email=$(jq -r '.utilisateur.email' "$json_file")
-  domain=$(jq -r '.utilisateur.domain' "$json_file")
-  password=$(jq -r '.utilisateur.password' "$json_file")
-  authMethod=$(jq -r '.utilisateur.traefik.authMethod' "$json_file")
-  domainperso=$(jq -r '.utilisateur.domainperso' "$json_file")
-  cloudflare_login=$(jq -r '.cloudflare.cloudflare_login' "$json_file")
-  cloudflare_api_key=$(jq -r '.cloudflare.cloudflare_api_key' "$json_file")
-  oauth_client=$(jq -r '.utilisateur.traefik.oauth_client' "$json_file")
-  oauth_secret=$(jq -r '.utilisateur.traefik.oauth_secret' "$json_file")
-  oauth_mail=$(jq -r '.utilisateur.traefik.oauth_mail' "$json_file")
+app_count=$(jq '.applications | length' $json_file)
 
-  # Mise à jour fichier all.yml avec les données
-  manage_account_yml user.name "$username"
-  manage_account_yml user.mail "$email"
-  manage_account_yml user.domain "$domain"
-  manage_account_yml user.pass "$password"
-  manage_account_yml cloudflare.login "$cloudflare_login"
-  manage_account_yml cloudflare.api "$cloudflare_api_key"
-  manage_account_yml sub.traefik.auth "$authMethod"
-  manage_account_yml sub.traefik.traefik "$domainperso"
+if [ "$app_count" -gt 0 ]; then
+    line=$1
 
-  if [ "$authMethod" == "oauth" ]; then
-    # Applique les paramètres OAuth
-    openssl=$(openssl rand -hex 16)
-    manage_account_yml oauth.openssl "$openssl"
-    manage_account_yml oauth.client "$oauth_client"
-    manage_account_yml oauth.secret "$oauth_secret"
-    manage_account_yml oauth.account "$oauth_mail"
-  fi
+    auth=$(jq -r --arg label "$line" '.dossiers.authentification[$label] // empty' $json_file)
+    domaine=$(jq -r --arg label "$line" '.dossiers.domaine[$label] // ""' $json_file)
 
-  # creation utilisateur
-  userid=$(id -u)
-  grpid=$(id -g)
-  # on reprend les valeurs du account.yml, juste au cas où
-  user=$(get_from_account_yml user.name)
-  pass=$(get_from_account_yml user.pass)
-  manage_account_yml user.htpwd $(htpasswd -nb $user $pass)
-  manage_account_yml user.userid "$userid"
-  manage_account_yml user.groupid "$grpid"
+    # Si l'utilisateur a déjà sélectionné une authentification (comme "Oauth"), ne pas remplacer par "Basique"
+    if [ -z "$auth" ]; then
+        auth="Basique"
+    else
+        echo "Info: pour $line."
+    fi
 
-# Installation traefik
-ansible-playbook ${SETTINGS_SOURCE}/includes/dockerapps/traefik.yml
+    if [ -z "$domaine" ]; then
+        echo "Info: "
+    fi
+else
+    exit 1
+fi
 
+if [[ "${line}" == "plex" ]]; then
+    token=$(jq -r '.updaters.plex.token // empty' $json_file)
+    login=$(jq -r '.updaters.plex.login // empty' $json_file)
+    password=$(jq -r '.updaters.plex.password // empty' $json_file)
+
+    # Si des valeurs manquent, fournir des valeurs par défaut
+    token="${token:-default-token}"
+    login="${login:-default-login}"
+    password="${password:-default-password}"
+
+    echo "Info: Token Plex par défaut utilisé" [ "$token" == "default-token" ] && echo " (Aucune valeur trouvée)"
+    echo "Info: Login Plex par défaut utilisé" [ "$login" == "default-login" ] && echo " (Aucune valeur trouvée)"
+    echo "Info: Password Plex par défaut utilisé" [ "$password" == "default-password" ] && echo " (Aucune valeur trouvée)"
+
+    manage_account_yml plex.token "$token"
+    manage_account_yml plex.ident "$login"
+    manage_account_yml plex.sesame "$password"
+fi
+
+manage_account_yml sub.${line}.${line} "$domaine"
+manage_account_yml sub.${line}.auth "$auth"
+
+if [[ -f "${SETTINGS_STORAGE}/vars/${line}.yml" ]]; then
+    ansible-playbook "${SETTINGS_SOURCE}/includes/dockerapps/generique.yml" --extra-vars "@${SETTINGS_STORAGE}/vars/${line}.yml"
+elif [[ -f "${SETTINGS_SOURCE}/includes/dockerapps/${line}.yml" ]]; then
+    ansible-playbook "${SETTINGS_SOURCE}/includes/dockerapps/${line}.yml"
+elif [[ -f "${SETTINGS_SOURCE}/includes/dockerapps/vars/${line}.yml" ]]; then
+    ansible-playbook "${SETTINGS_SOURCE}/includes/dockerapps/generique.yml" --extra-vars "@${SETTINGS_SOURCE}/includes/dockerapps/vars/${line}.yml"
+else
+    log_write "Aucun fichier de configuration trouvé dans les sources, abandon"
+    error=1
+fi
+
+exit 0
